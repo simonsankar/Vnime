8fbc47beb3edc1db16946c5d93a6f94e605b0044
Simon Sankar <simon.sankar@my.uwi.edu>
2018-10-23 16:08:08 -0400

    Added footer barebones

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,10 @@
-const electron = require('electron');
+const electron = require("electron");
 // Module to control application life.
 // Module to create native browser window.
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, shell } = electron;
 
-const path = require('path');
-const url = require('url');
+const path = require("path");
+const url = require("url");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -23,14 +23,14 @@ function createWindow() {
     resizeable: true,
     minimizable: true,
     maximizable: true,
-    title: 'Vnime'
+    title: "Vnime"
   });
   // load the index.html of the app.
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, '/../build/index.html'),
-      protocol: 'file:',
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
       slashes: true
     });
   mainWindow.loadURL(startUrl);
@@ -39,32 +39,39 @@ function createWindow() {
   // mainWindow.webContents.openDevTools({ detach: true });
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+  });
+
+  mainWindow.webContents.on("will-navigate", event => {
+    console.log(event);
+    // event.preventDefault();
+    // let link = event.target.href;
+    // shell.openExternal(link);
   });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
-  console.log('App running...');
+  console.log("App running...");
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import {
   getPopularAndTrendingAnimes,
   resetPopularAndTrendingAnimes
-} from '../actions/getPopularAndTrendingAnimes';
+} from "../actions/getPopularAndTrendingAnimes";
 
-import { Grid, Divider } from 'semantic-ui-react';
-import SampleList from './SampleList';
+import { Grid, Divider, List } from "semantic-ui-react";
+import SampleList from "./SampleList";
 
 class Home extends Component {
   componentWillMount() {
@@ -49,7 +49,7 @@ class Home extends Component {
               <SampleList text="Popular Today" animes={sampleAnimes.popular} />
             </div>
           ) : (
-            ''
+            ""
           )}
           <Divider />
           {sampleAnimes !== null ? (
@@ -60,8 +60,55 @@ class Home extends Component {
               />
             </div>
           ) : (
-            ''
+            ""
           )}
+        </Grid.Row>
+        <Grid.Row className="footer" columns={3}>
+          <Grid.Column textAlign="center">
+            <List>
+              <List.Item>
+                <List.Icon name="hand peace" />
+                <List.Content>Vnime</List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="marker" />
+                <List.Content>D'Abadie, TT</List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="mail" />
+                <List.Content>
+                  <a href="mailto:sjsankar10@gmail.com">sjsankar10@gmail.com</a>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="github" />
+                <List.Content>
+                  <a href="https://github.com/simonsankar/Vnime">
+                    github.com/simonsankar/vnime
+                  </a>
+                </List.Content>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column textAlign="center">
+            <List>
+              <List.Item>
+                <List.Icon name="react" />
+                <List.Content>ReactJS</List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="js" />
+                <List.Content>ElectronJS</List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="database" />
+                <List.Content>Firebase</List.Content>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column textAlign="center">
+            <p>Blah blah</p>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     );
