96b813054d78f609876ec439861565cbb2c06c80
simonsankar <simonlfcsuarez7@gmail.com>
2018-10-23 20:15:38 -0400

    Filter function

---
+++ src/common/filterOptions.js
@@ -0,0 +1,21 @@
+export const orderOptions = [
+  { name: 'A - Z', id: 'title' },
+  { name: 'Z - A', id: 'title_desc' },
+  { name: 'Lowest', id: 'score' },
+  { name: 'Highest', id: 'score_desc' }
+];
+
+export const typeOptions = [
+  { name: 'TV', id: '0' },
+  { name: 'Movie', id: '1' },
+  { name: 'OVA', id: '2' },
+  { name: 'Special', id: '3' },
+  { name: 'ONA', id: '4' }
+];
+
+export const statusOptions = [
+  { name: 'Ongoing', id: '0' },
+  { name: 'Completed', id: '1' },
+  { name: 'Not aired yet', id: '2' }
+];
+export const genreOptions = [{ name: 'Action', id: 57 }];
