47467004ecd3187f00942dd749b9ae400097de87
Simon Sankar <sjsankar10@gmail.com>
2018-11-08 12:54:30 -0400

    Scroll to top fix

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,30 +1,31 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { getAnime, resetAnime } from '../actions/getAnime';
-import { resetEpisodeOptions } from '../actions/getEpisodeOptions';
-import { resetVideo } from '../actions/selectVideo';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { getAnime, resetAnime } from "../actions/getAnime";
+import { resetEpisodeOptions } from "../actions/getEpisodeOptions";
+import { resetVideo } from "../actions/selectVideo";
 
-import { Grid, Segment, Dimmer, Loader } from 'semantic-ui-react';
-import Hero from './Hero';
-import SideContent from './SideContent';
-import EpisodeList from './EpisodeList';
-import VideoPlayer from './VideoPlayer';
-import VideoBar from './VideoBar';
+import { Grid, Segment, Dimmer, Loader } from "semantic-ui-react";
+import Hero from "./Hero";
+import SideContent from "./SideContent";
+import EpisodeList from "./EpisodeList";
+import VideoPlayer from "./VideoPlayer";
+import VideoBar from "./VideoBar";
 
 class Watch extends Component {
   componentDidMount() {
     const { pathname } = this.props.location;
-    this.props.getAnime(pathname.replace('/watch/', ''));
+    this.props.getAnime(pathname.replace("/watch/", ""));
   }
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
       this.props.resetVideo();
       this.props.resetAnime();
       this.props.resetEpisodeOptions();
       const { pathname } = this.props.location;
-      this.props.getAnime(pathname.replace('/watch/', ''));
+      this.props.getAnime(pathname.replace("/watch/", ""));
     }
   }
   componentWillUnmount() {
@@ -36,13 +37,7 @@ class Watch extends Component {
   render() {
     const { anime } = this.props;
     return (
-      <div
-        style={{
-          backgroundColor: '#f2f8fc',
-          minHeight: '180vh',
-          paddingBottom:'40px'
-        }}
-      >
+      <div className="page-watch">
         {anime !== null && anime !== false ? (
           <div>
             {/* Hero */}
