24ae1da2684ccfe839500a64748b3afb7175168d
Simon Sankar <sjsankar10@gmail.com>
2018-11-21 08:44:55 -0400

    Popup for AnimeItem

--- src/components/AnimeItem.js
+++ src/components/AnimeItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Grid, Card, Image, Icon } from "semantic-ui-react";
+import { Grid, Card, Image, Icon, Popup } from "semantic-ui-react";
 
 const AnimeItem = ({ anime }) => {
   return (
@@ -12,26 +12,35 @@ const AnimeItem = ({ anime }) => {
       widescreen={2}
       className="fade-in"
     >
-      <Card fluid as={Link} to={anime.url} className="card-no-border">
-        <Image rounded src={anime.anime.poster} fluid />
-        {anime.episode && (
-          <div className="card-episodes">
-            <span>EP {anime.episode}</span>
-          </div>
-        )}
-        {anime.extra && (
-          <div className="card-extra">
-            <span>
-              <Icon name="eye" size="small" />
-              {anime.extra}
-            </span>
-          </div>
-        )}
-        <div className="card-overlay">{anime.anime.title}</div>
-        <div className="card-play">
-          <Icon name="video play" size="huge" />
-        </div>
-      </Card>
+      <Popup
+        basic
+        content={anime.anime.title}
+        hideOnScroll
+        inverted
+        size="mini"
+        trigger={
+          <Card fluid as={Link} to={anime.url} className="card-no-border">
+            <Image rounded src={anime.anime.poster} fluid />
+            {anime.episode && (
+              <div className="card-episodes">
+                <span>EP {anime.episode}</span>
+              </div>
+            )}
+            {anime.extra && (
+              <div className="card-extra">
+                <span>
+                  <Icon name="eye" size="small" />
+                  {anime.extra}
+                </span>
+              </div>
+            )}
+            <div className="card-overlay">{anime.anime.title}</div>
+            <div className="card-play">
+              <Icon name="video play" size="huge" />
+            </div>
+          </Card>
+        }
+      />
     </Grid.Column>
   );
 };
